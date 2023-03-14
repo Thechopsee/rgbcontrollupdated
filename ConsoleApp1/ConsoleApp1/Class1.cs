e5a1e4ed6f21d507a2bb095a0b811a386e3c8df7
Thechopsee <sebastian.walent@gmail.com>
2023-03-14 23:00:30 +0100

    colore zemplate

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using Colore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ColoreColor = Colore.Data.Color;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        IChroma chroma;
+        public async void button1_Click()
+        {
+            // Sets all Chroma devices to red, the 'chroma' variable should be saved
+            // somewhere that is globally accessible to the application.
+
+            chroma = await ColoreProvider.CreateNativeAsync();
+
+            Console.WriteLine(chroma.SdkVersion);
+            await chroma.SetAllAsync(ColoreColor.Red);
+
+            // Without the usings it will look like this:
+            // var chroma = await Colore.ColoreProvider.CreateNativeAsync();
+            // await chroma.SetAllAsync(Colore.Data.Color.Red);
+        }
+        public async void nevim()
+        {
+            Console.WriteLine(chroma.SdkVersion);
+            Console.WriteLine(chroma.Initialized) ;
+            await chroma.SetAllAsync(ColoreColor.Red);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Colore;
+using ConsoleApp1;
+using System;
+
+
+namespace ConsoleApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Class1 cs = new Class1();
+            cs.button1_Click();
+
+            while (true)
+            {
+                cs.nevim();
+            }
+        }
+    }
+}
